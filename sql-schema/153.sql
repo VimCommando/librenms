3e9c48cdd647fa49800bd8fe0b579922115f098c
Ryan Eno <ryaneno@gmail.com>
2016-12-15 16:51:03 +0000

    Fixed sql-schema/153.sql (commited wrong version of file)

--- sql-schema/153.sql
+++ sql-schema/153.sql
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
 INSERT INTO `graph_types`(`graph_type`, `graph_subtype`, `graph_section`, `graph_descr`, `graph_order`) VALUES ('device',  'mtxr_hotspot_users',  'system',  'Active Hotspot Users',  '');
-=======
-INSERT INTO `graph_types`(`graph_type`, `graph_subtype`, `graph_section`, `graph_descr`, `graph_order`) VALUES ('device',  'sgos_average_requests',  'network',  'Average HTTP Requests',  '');
-
->>>>>>> 969b963180ea42272c5b9c40851c683daf1c6337
